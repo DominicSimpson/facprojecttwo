0855c0a9e356b809935e088574d56dac2dfb2eae
Dominic Simpson <52511353+DominicSimpson@users.noreply.github.com>
2022-07-31 19:46:05 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,18 @@ const dotFour = document.getElementById("dotFour");
 const dotFive = document.getElementById("dotFive");
 const dotSix = document.getElementById("dotSix");
 
+const showDescription = document.getElementById("showDescription");
+const showSecondDescription = document.getElementById("showSecondDescription");
+const showThirdDescription = document.getElementById("showThirdDescription");
+const showFourthDescription = document.getElementById("showFourthDescription");
+const showFifthDescription = document.getElementById("showFifthDescription");
+const showSixthDescription = document.getElementById("showSixthDescription");
+const showSeventhDescription = document.getElementById("showSeventhDescription");
+const showEighthDescription = document.getElementById("showEighthDescription");
+const showNinthDescription = document.getElementById("showNinthDescription");
+const showTenthDescription = document.getElementById("showTenthDescription");
+
+
 
 prevSlider.addEventListener("click", function (){
   showSlides(slideIndex += -1);
@@ -41,6 +53,96 @@ dotFive.addEventListener("click", function (){
 
 dotSix.addEventListener("click", function (){
   showSlides(slideIndex = 6);
+});
+
+showDescription.addEventListener("click", function (){
+  var x = document.getElementById("cardText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+showSecondDescription.addEventListener("click", function (){
+  var x = document.getElementById("cardTwoText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+showThirdDescription.addEventListener("click", function (){
+  var x = document.getElementById("cardThirdText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+showFourthDescription.addEventListener("click", function (){
+  var x = document.getElementById("cardFourthText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+showFifthDescription.addEventListener("click", function (){
+  var x = document.getElementById("cardFifthText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+showSixthDescription.addEventListener("click", function (){
+  var x = document.getElementById("cardSixthText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+showSeventhDescription.addEventListener("click", function (){
+  var x = document.getElementById("cardSeventhText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+showEighthDescription.addEventListener("click", function (){
+  var x = document.getElementById("cardEighthText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+showNinthDescription.addEventListener("click", function (){
+  var x = document.getElementById("cardNinthText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+});
+
+showTenthDescription.addEventListener("click", function (){
+  var x = document.getElementById("cardTenthText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 });
 
 let slideIndex = 1; // initalised variable for slide index
